a036a9c318444441ab08f7e24f2a075b1ab11e12
elanonc <nunes.elano14@gmail.com>
2024-01-15 20:31:43 -0300

    fix: ajuste nos posicionamentos

--- portfolio/src/pages/home/home.tsx
+++ portfolio/src/pages/home/home.tsx
@@ -8,11 +8,10 @@ const PortfolioHome: React.FC = () => {
   return (
     <>
         <PortfolioNavbar/>
-        <CustomTitle text="I do code and make content about it!" size="55px"/>
-        <CustomText text="I am a seasoned full-stack software engineer with over
-            8 years of professional experience, specializing in backend development.
-            My expertise lies in crafting robust and scalable SaaS-based
-            architectures on the Amazon AWS platform." size="18px"/>
+        <div className='home-text-container'>
+          <CustomTitle text="I do code and make content about it!" size="55px"/>
+          <CustomText text="I am a seasoned full-stack software engineer with over 8 years of professional experience, specializing in backend development. My expertise lies in crafting robust and scalable SaaS-based architectures on the Amazon AWS platform." size="18px"/>
+        </div>
     </>
   );
 };
