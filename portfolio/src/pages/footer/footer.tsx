24ea7657044ea393069a644264012a59663fd596
elanonc <nunes.elano14@gmail.com>
2024-01-22 22:49:54 -0300

    ajustes na seção do footer

--- portfolio/src/pages/footer/footer.tsx
+++ portfolio/src/pages/footer/footer.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import "./footer.css";
 import email from '../../assets/email.svg'
+import CustomTitle from '../../components/title/title';
+import CustomText from '../../components/text/text';
 
 const PortfolioFooter: React.FC = () => {
   return (
-    <footer>
-        <h4>Contact</h4>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque quis, est magnam porro quae ducimus. Eos excepturi assumenda, placeat ab illo magnam provident dolores, sed culpa maxime adipisci sint omnis!
-        </p>
+    <footer className='footer'>
+      <div className="footer-container">
+        <CustomTitle text="Contact" size="24px"/>
+        <CustomText text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque quis, est magnam porro quae ducimus. Eos excepturi assumenda, placeat ab illo magnam provident dolores, sed culpa maxime adipisci sint omnis!" size="15px"/>
         <div className="email-container">
           <img src={email} alt="icon de email" />
+          <p>nunes.elano14@gmail.com</p>
         </div>
         <div className="social-medias-container">
 
         </div>
+      </div>
     </footer>
   );
 };
